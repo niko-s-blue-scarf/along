cfe1fcb7094993afcd1e2248c5c16d592ed865fb
CoCrystal <3784544633@qq.com>
2026-07-12 08:58:12 +0800

    显示2026-7-12

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import time
+import os
 class 主要():
     def __init__(self):
         self.分辨率=8
         self.画面 = [["、"] * self.分辨率 for _ in range(self.分辨率)]
     def 显像(self):
-        print("\n"*(self.分辨率*4))
+        self.清屏()
         for 指针 in self.画面:
             for 指针2 in 指针:
                 print(指针2,end="")
             print()
+    def 添加元素(self,坐标,元素):
+        self.画面[坐标[0]][坐标[1]]=str(元素)
+    def 清屏(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
     def 运行(self):
+        while True:
+            操作=input(">")
+            try:
+                exec(操作)
+            except:
+                print("执行失败")
+            self.显像()
+        """
         while True:
             self.显像()
             time.sleep(0.1)
+        """
 主体=主要()
 主体.运行()
